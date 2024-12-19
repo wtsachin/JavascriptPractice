4a101941019925f71542144511214e2cd5cccb70
wtsachin <sachin.kolhe@wargstech.com>
2024-12-19 22:41:47 +0530

    conversion of datatypes

---
+++ 03_conversionOperationn.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLogged)
+
+// 1 => true 0 => false
+// "" => false
+// "hitesh" => true
