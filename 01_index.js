5daa500de1817795d28b385106cb82324dcbc423
wtsachin <sachin.kolhe@wargstech.com>
2024-12-19 22:05:26 +0530

    added dataTypes

---
+++ 01_index.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+alert("Hello") //we re using node js not browser
+
+const accountId = 14453
+let accountEmail = "sachin@gmail.com"
+let accountPassword = "123456"
+accountCity = "Pune"
+
+// accountId = 3
+
+/*
+Prefer not to use var 
+Bcause of isue in blocked scope and functional scope  
+*/
+
+console.table([accountId, accountEmail, accountPassword])
+console.log(accountId);
---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// alert("Hello")
+
+console.log(3 + 3)
+
+console.log("Sachin")
+
+// code readabilty should be high
+
+let name = "Sachin"
+let age = 18
+let isLoggedIn = false
+
+
+// number  2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+// object 
+
+console.log(typeof null) 
+//object
+console.log(typeof undefined)
+//undefined
+
