a621e233aeb86e43be5635d5e325af40e456a682
wtsachin <sachin.kolhe@wargstech.com>
2024-12-19 23:14:42 +0530

    conversion opertor

--- 03_conversionOperationn.js
+++ 03_conversionOperationn.js
@@ -1,12 +1,12 @@
 let score = "33"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
 
-console.log(valueInNumber)
-console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
 
 // "33"=> 33
 // "33abc" => NaN
@@ -14,8 +14,35 @@ console.log(typeof valueInNumber)
 
 let isLoggedIn = 1;
 let booleanIsLogged = Boolean(isLoggedIn)
-console.log(booleanIsLogged)
+// console.log(booleanIsLogged)
 
 // 1 => true 0 => false
 // "" => false
 // "hitesh" => true
+
+
+// ************************************ Operations ****************************************
+let value = 3
+let nagValue = -value
+// -3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Sachin"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + "2" + 1)
+console.log(1 + 2 +"2")
+
+console.log(+true)
+// 1
+console.log(+"")
